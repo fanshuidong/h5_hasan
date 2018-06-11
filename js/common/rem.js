b74c7392f0549116ac9e1ad71d120424b52829a0
yangyang <yangyang@weiqianjin.cn>
2018-06-11 14:46:00 +0800

    fsd

--- js/common/rem.js
+++ js/common/rem.js
@@ -1,5 +1,5 @@
 var s = document.documentElement.clientWidth;
 document.documentElement.style.fontSize = s/25+ "px";
-// var host = "http://manager.hassanchina.com";
+var host = "http://manager.hassanchina.com";
 // var host = "http://localhost:8089";
-var host = "http://172.16.20.105:8080";
+// var host = "http://172.16.20.105:8080";
